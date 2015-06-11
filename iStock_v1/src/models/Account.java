62490126f4592d725f3d2746cbe2b65dee9d2b1e
Chenrenzhan <chenrenzhan@gmail.com>
2015-06-11 15:06:21 +0800

    修改账号存储的数据结构

--- iStock_v1/src/models/Account.java
+++ iStock_v1/src/models/Account.java
@@ -14,80 +14,95 @@ import controller.StockMath;
 public class Account {
 	private final static String FILEPATH = "data/accounts.json";
 	private JSONObject accountJsonObject;
+	private JSONArray accountArray;
+//	private ArrayList<String> accountList;
 
 	public Account() {
-		accountJsonObject = toJsonObject(IORW.read(FILEPATH));
-	}
-
-	public ArrayList<String> getAccounts() {
-		int len = 0;
-		ArrayList<String> strArray = new ArrayList<String>();
-		Iterator<?> keys1 = accountJsonObject.keys();
-		// 计数记录数目
-		while (keys1.hasNext()) {
-			String key = (String) keys1.next().toString();
-			strArray.add(key);
-
-		}
-		System.out.println(len);
-		
-//		int row = 0;
-//		Iterator<?> keys2 = accountJsonObject.keys();
-//		while (keys2.hasNext()) {// 遍历JSONObject
-//			String key = (String) keys2.next().toString();
-//			JSONArray ja = accountJsonObject.getJSONArray(key);
-//
-//			// 遍历数组
-//			for (int i = 0; i < ja.length(); i++) {
-//				JSONObject rd = (JSONObject) ja.get(i);
-//				// for(int j = 1; j < KEYS.length; ++j){System.out.println(j);
-//				// strArray[row++][j] = rd.getString(KEYS[j]);
-//				// }
-//				strArray[row][0] = rd.getString("name");
-//				++row;
-//			}
-//			
-//		}
-		return strArray;
-
-	}
-
-	public void addAccount(String acc) {
+		String str = IORW.read(FILEPATH);
+		str = empty(str);
 		try {
-			accountJsonObject.put(acc, "");
+			accountArray = new JSONArray(str);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private String empty(String str){
+		if(str.isEmpty() || str.equals("{}")){
+			str = "[]";
+		}
+		return str;
+	}
+	
+	public ArrayList<String> getAccounts() {
+		int len = accountArray.length();
+		ArrayList<String> accountList = new ArrayList<String>();
+		for(int i = 0; i < len; ++i){
+			String name;
+			try {
+				name = accountArray.getString(i);
+				accountList.add(name);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return accountList;
+
+	}
+
+	public void addAccount(String acc) {
+		accountArray.put(acc);
 		save();
 	}
 
 	public void deleteAccount(String acc) {
-		accountJsonObject.remove(acc);
+		JSONArray ja = new JSONArray();
+		int len = accountArray.length();
+		for(int i = 0; i < len; ++i){
+			String account = "";
+			try {
+				account = (String) accountArray.get(i);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(account.equals(acc)){
+				continue;
+			}
+			else{
+				ja.put(account);
+			}
+		}
+		accountArray = null;
+		accountArray = ja;
 		save();
 	}
 
 	public void save(){
 		try {
-			IORW.write(FILEPATH, accountJsonObject.toString());
+			IORW.write(FILEPATH, accountArray.toString());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	private JSONObject toJsonObject(String jsonStr) {
-		JSONObject accJO;
-		if (jsonStr == null) {
-			jsonStr = "{}";
-
-		}
-		try {
-			accJO = new JSONObject(jsonStr);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Accounts.json is null");
-			accJO = new JSONObject();
-		}
-		return accJO;
-	}
+//	private JSONObject toJsonObject(String jsonStr) {
+//		JSONArray accJO;
+//		if (jsonStr == null) {
+//			jsonStr = "[]";
+//
+//		}
+//		try {
+//			accJO = new JSONObject(jsonStr);
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			System.out.println("Accounts.json is null");
+//			accJO = new JSONObject();
+//		}
+//		return accJO;
+//	}
 }
